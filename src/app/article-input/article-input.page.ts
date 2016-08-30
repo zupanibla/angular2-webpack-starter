5e73f78706033aeaf59fcdb066a8e58ea7f7d1f0
Blush <zupanibla@gmail.com>
2016-08-30 12:54:04 +0200

    Added some ugly content to ArticleInputPage

--- src/app/article-input/article-input.page.ts
+++ src/app/article-input/article-input.page.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
 
-@Component({
-	template: '<h1>ArticleInputPage</h1>'
+// import { RtfInputComponent } from './components/rtf-input.component';
+
+@Component({ // TODO lepsi template
+	template: `
+	    <div class="container">
+	        <div class="col-md-2">
+	        </div>
+	        <div class="col-md-8" style="overflow: hidden;">
+	            <div id="main">
+	                <div class="page-header">
+	                    <h1>
+	                    Članker<br>
+	                    <small>Inovativna rešitev za domače naloge pri angleščini</small>
+	                    </h1>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-md-2">
+	        </div>
+	    </div>
+	    <footer>
+	        <a
+	         href="javascript: void(alert('Pejd do Blaža pa ti povedu o Aplikaciji za članke'))"
+	        >o Aplikaciji za clanke</a> 
+	        | 
+	        <a href="mailto:zupaniblaz@gmail.com">kontaktirajte nas</a>
+	    </footer>
+	`,
+	styleUrls: ['./styles/article-input.style.css'],
+	// directives: [RtfInputComponent]
 })
 export class ArticleInputPage {
-
+	constructor() {}
 }

5ea605c47ea1279b2621f7e276daf369068bcfc8
Blush <zupanibla@gmail.com>
2016-09-17 00:29:57 +0200

    Revert "Implemented output on 'AdaptedMarkableComponent'"
    This reverts commit 4c5ca8598b665545281cc427a19938e47b391c40.

--- src/app/word-marker/components/adapted-markable-word.component.ts
+++ src/app/word-marker/components/adapted-markable-word.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectorRef, Input, Output } from '@angular/core';
 import { MarkableWordComponent } from './../components/markable-word.component';
 
 @Component({
@@ -8,13 +8,11 @@ import { MarkableWordComponent } from './../components/markable-word.component';
 })
 export class AdaptedMarkableWordComponent {
 	@Input() set content(val: string) { this._content = val; this._cd.detectChanges(); }
-	@Output() mark: EventEmitter<boolean> = new EventEmitter<boolean>();
 	private _content: string = '';
 	
 	constructor(private _cd: ChangeDetectorRef) {}
 	
 	private markHandler(e) {
-		this.mark.emit(e);
 		this._cd.detectChanges();
 	}
 }

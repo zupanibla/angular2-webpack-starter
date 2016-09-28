93d862a9c549657dfdfdf16931ede3da9dbe08ac
Blush <zupanibla@gmail.com>
2016-09-28 21:20:15 +0200

    Added onChange to Set

--- src/app/shared/utils/set.util.ts
+++ src/app/shared/utils/set.util.ts
@@ -1,12 +1,18 @@
+import { Observable, Subject } from 'rxjs'; // TODO poprav _ terminologijo
+
 export class Set<E> {
 	private _store: Object = {};
+	private _onChange: Subject<any> = new Subject<{element:string,exists:boolean}>();
+	public onChange: Observable<{element:string,exists:boolean}> = this._onChange.asObservable();
 
 	add(element: E) {
 		this._store[(<any>element)] = true;
+		this._onChange.next({element, exists: true});
 	}
 
 	remove(element: E) {
 		delete this._store[(<any>element)];
+		this._onChange.next({element, exists: false});
 	}
 
 	toggle(element: E) {

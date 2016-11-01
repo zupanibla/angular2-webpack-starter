7cff280b2124efd90e2b2de50c4706df7ef44a08
Blush <zupanibla@gmail.com>
2016-11-01 16:14:48 +0100

    TODO new SettingsComponent template

--- src/app/pages/word-marker/components/settings.component/settings.component.ts
+++ src/app/pages/word-marker/components/settings.component/settings.component.ts
@@ -28,3 +28,37 @@ export class SettingsComponent {
 
 	constructor(private settings: SettingsService) {}
 }
+
+// TODO flikn v form
+// import { Component } from '@angular/core';
+
+// import { SettingsService } from './../../services/settings.service';
+
+// @Component({
+// 	selector: 'settings',
+// 	template: `
+// 	Format izgovorjave: <br>
+// 	<div class="btn-group" data-toggle="buttons">
+// 		<label class="btn btn-default"
+// 		 [class.active]="settings.pronunciationFormat == 'ipa'"
+// 		 (click)="settings.pronunciationFormat = 'ipa'"
+// 		>IPA</label>
+// 		<input type="radio" autocomplete="off">
+// 		<label class="btn btn-default"
+// 		 [class.active]="settings.pronunciationFormat == 'spell'"
+// 		 (click)="settings.pronunciationFormat = 'spell'"
+// 		>Spell</label>
+// 		<input type="radio" autocomplete="off">
+// 	</div>
+// 	<div class="checkbox">
+// 		<label>
+// 			<input type="checkbox" [(ngModel)]="settings.showExamples"> 
+// 			Prikaži primere uporabe
+// 		</label>
+// 	</div>
+// 	`
+// })
+// export class SettingsComponent {
+
+// 	constructor(private settings: SettingsService) {}
+// }

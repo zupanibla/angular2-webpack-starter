5fdf8a89433c95878faf799024eb873d42b733d6
Blush <zupanibla@gmail.com>
2016-11-13 14:43:54 +0100

    IPA is now default pronunciation format

--- src/app/pages/word-marker/services/settings.service.ts
+++ src/app/pages/word-marker/services/settings.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SettingsService {
-	pronunciationFormat: string = "spell";
+	pronunciationFormat: string = "ipa";
 	showExamples: boolean       = true;
 }

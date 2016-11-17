8c9ef4f69d424e67df0b1827a6239a2425e1757d
Blush <zupanibla@gmail.com>
2016-11-17 19:15:56 +0100

    New DictionaryService with states

--- src/app/pages/word-marker/components/word-list.component/item.component/item.component.ts
+++ src/app/pages/word-marker/components/word-list.component/item.component/item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { DictionaryKey } from './../../../../../shared/structures/dictionary-key.structure';
-import { DictionaryService } from './../../../services/dictionary.service';
+import { DictionaryService, DictionaryEntry, DictionaryEntryState } from './../../../services/dictionary.service';
 import { WordCardComponent } from './../../../components/word-card.component';
 
 @Component({
@@ -9,21 +9,42 @@ import { WordCardComponent } from './../../../components/word-card.component';
 	template: `
 	<div class="wrapper">
 		<div class="remove-wrapper"><span class="remove" (click)="delete.emit(true)"></span></div>
-		<div *ngIf="!_data">Loading...</div>
-		<div><word-card *ngIf="_data" [data]="_data"></word-card></div>
+		
+		<div *ngIf="dictionaryEntry.state === State.LOADING">Loading...</div>
+		<div *ngIf="dictionaryEntry.state === State.READY">
+			<word-card [data]="definitionCardFromEntry(dictionaryEntry, dictionaryKey)"></word-card>
+		</div>
+		<div *ngIf="dictionaryEntry.state === State.FAILURE">Failed to load word <b>{{dictionaryKey.query}}</b> 
+			<a href="javascript:void(0)" (click)="reload()">Try Reloading?</a>
+		</div>
 	</div>
 	`,
 	styleUrls: ['item.component.style.sass'],
 	directives: [WordCardComponent]
 })
 export class WordListItemComponent {
+	State = DictionaryEntryState;
+
 	@Input() dictionaryKey: DictionaryKey;
 	@Output() delete: EventEmitter<boolean> = new EventEmitter<boolean>();
-	private _data = null;
+	private dictionaryEntry: DictionaryEntry = null;
 
-	constructor(private _dictionary: DictionaryService) {}
+	constructor(private dictionary: DictionaryService) {}
 
 	ngOnInit() {
-		this._dictionary.getDefinitionCard(this.dictionaryKey).subscribe(data => { this._data = data });
+		console.log('A');
+		this.dictionary.getEntry(this.dictionaryKey.query).subscribe(data => { this.dictionaryEntry = data; });
+		console.log('B');
+	}
+
+	private definitionCardFromEntry(entry: DictionaryEntry, key: DictionaryKey) { // TODO tole je shit
+			let word = entry.data[key.wordNumber];
+			console.log(word);
+			return {
+				text: word.text,
+				pronunciation: word.pronunciation,
+				usageType: word.usages[key.usageNumber].type,
+				meaning: word.usages[key.usageNumber].meanings[key.meaningNumber]
+			}
 	}
 }
--- src/app/pages/word-marker/services/dictionary.service.ts
+++ src/app/pages/word-marker/services/dictionary.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
+import { HashMap } from './../../../shared/utils/hash-map.util';
 import { DictionaryKey } from './../../../shared/structures/dictionary-key.structure';
 import { DictionaryAjaxRequestsService } from './../services/dictionary-ajax-requests.service';
 import { Word } from './../structures/word.structure';
 
 @Injectable()
 export class DictionaryService {
+	words: HashMap<string, Observable<DictionaryEntry>> = new HashMap<string, Observable<DictionaryEntry>>();
 
-	constructor(private dictionaryAjaxRequests: DictionaryAjaxRequestsService) {} // TODO Kje je cache
+	constructor(private dictionaryAjaxRequests: DictionaryAjaxRequestsService) {}
 
-	getWords(query: string): Observable<Array<Word>> {
-		return this.dictionaryAjaxRequests.requestGetWordsByQuery(query);
+	getEntry(query: string): Observable<DictionaryEntry> { // TODO tole je shit
+		if (!this.words.has(query)) {
+			let subject = new BehaviorSubject<DictionaryEntry>({ data: null, state: DictionaryEntryState.LOADING });
+			this.words.set(query, subject);
+		}
+		this.loadEntry(query);
+		return this.words.get(query);
 	}
 
-	getDefinitionCard(key: DictionaryKey): Observable<any> { // TODO tole je shit
-		return this.getWords(key.query).map(words => {
-			let word = words[key.wordNumber];
-			return {
-				text: word.text,
-				pronunciation: word.pronunciation,
-				usageType: word.usages[key.usageNumber].type,
-				meaning: word.usages[key.usageNumber].meanings[key.meaningNumber]
-			}
-		});
+	private loadEntry(query: string) {
+		this.dictionaryAjaxRequests.requestGetWordsByQuery(query).subscribe(
+			data => { this.words.get(query).next({ data, state: DictionaryEntryState.READY }); },
+			()   => { this.words.get(query).next({ data: null, state: DictionaryEntryState.FAILURE })}
+		);
 	}
+
+	reloadEntry(query: string) { this.loadEntry(query); }
+}
+
+export interface DictionaryEntry {
+	data?: Array<Word>;
+	state: DictionaryEntryState;
+}
+
+export enum DictionaryEntryState {
+	READY, LOADING, FAILURE
 }

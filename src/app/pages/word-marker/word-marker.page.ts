133f3177e228549413c8207e7b59183e0339ce60
Blush <zupanibla@gmail.com>
2017-01-02 21:58:24 +0100

    Separated word-marker.page-template from ts

--- src/app/pages/word-marker/word-marker.page.ts
+++ src/app/pages/word-marker/word-marker.page.ts
@@ -10,50 +10,14 @@ import { PerspectiveService, ViewType } from './../../shared/services/perspectiv
 import { DefinitionSelectionModalComponent } from './components/definition-selection-modal.component';
 
 @Component({
-	template: `
-		<div class="container">
-	        <div class="col-md-2">
-	        	<div class="menu-wrapper" [class.affix]="perspective.viewType === ViewType.DESKTOP">
-			        <div class="menu">
-			        	<a href="javascript:void(0)" (click)="settingsModal.open()" class="menu-item">
-			        		<i class="fa fa-cog fa-3x"></i>
-			        	</a>
-			        	<a href="javascript:void(0)" (click)="printArticle()" class="menu-item">
-			        		<i class="fa fa-print fa-3x"></i>
-			        	</a>
-			        	<settings-modal #settingsModal></settings-modal>
-			        </div>
-		        </div>
-	        </div>
-	        <div class="col-md-8">
-	            <div id="main">
-					<article-panel [article]="article"
-					 (wordToggle)="(perspective.viewType === ViewType.MOBILE && $event.marked) 
-					               ? openDefinitionSelectionModal($event.id) : false"
-					 ></article-panel>
-	            </div>
-	        </div>
-	        <div class="col-md-2 word-list-col">
-	        	<div class="word-list-wrapper"
-	        	 [class.affix]="perspective.viewType === ViewType.DESKTOP"
-	        	>
-					<word-list
-					 [words]="markedWords"
-					 (wordClick)="openDefinitionSelectionModal($event.id)"
-					 (wordDelete)="articleService.unmarkWord($event.id)"
-					></word-list>
-				</div>
-	        </div>
-	    </div>
-	    <definition-selection-modal #definitionSelectionModal></definition-selection-modal>
-	`,
+	templateUrl: './word-marker.page.template.html',
 	directives: [ArticlePanelComponent, WordListComponent, SettingsModalComponent, DefinitionSelectionModalComponent],
 	styleUrls: ['./word-marker.page.style.sass']
 })
 export class WordMarkerPage {
 	ViewType = ViewType;
 	article: Article;
-	console = console;
+
 	constructor(router: Router, public articleService: ArticleService,
 	 cdr: ChangeDetectorRef, private perspective: PerspectiveService) {
 		console.log('!WordMarkerPage.constructor');
@@ -69,9 +33,7 @@ export class WordMarkerPage {
 			this.article.wordDefinitions[wordId] = dictionaryKey;
 		});
 	}
-	// TODO: ARTICLE PANEL ?
-
-	// MARKED WORDS LIST
+	
 	private get markedWords() {
 		return this.article.markedWordsIds.toArray().map(str=>parseInt(str)).map(wordId => {
 			return { wordId, dictionaryKey: this.article.wordDefinitions[wordId]}
